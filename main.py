ea9ad62905aba89d8b12a03fad66d5bc848537bb
anditiana <51283033+anditiana@users.noreply.github.com>
2024-02-13 07:45:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#Data Types
+
+#String
+"Hello"
+print("Hello")
+
+"Hello"[4]
+#mean what charcter in index 4? o
+#this method only for string, otherwise error
+print("Hello"[4])
+
+#integer
+123
+print(123)
+#to use addition(+), substraction(-), multiple(x), or div(/)
+print(123+123)
+print(134*123)
+print(144-12)
+print(90/3)
+#and to add comas, or point each thousan use 123_000 instead 123,000
+
+float
+print(123.124)
+
+# Boolean
+# there only two values in boolean
+True 
+False
+
+print(True)
+print(False)
