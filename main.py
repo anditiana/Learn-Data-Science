9e2b944a859a54d2e9d67622c5d45970991dfb1e
leavcoder <13051262-leavcoder@users.noreply.replit.com>
2024-02-19 07:51:30 +0000

    day 3
    primitive data, type data, data collection

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+"""
+primitve data
+integer,
+float,
+complex (e.g: 1 + 2j)
+"""
+
+#string
+name = "perseus evans"
+print(name)
+#there is 3 method to call string like concatenate
+#1. Formatted string
+print(f"hello guys, my name {name}\n")
+#2. %-Formatting
+print("my name %s \n" %name)
+#3. str.format
+print("my name is {} \n".format(name))
+
+
+#data collection
+
+#list
+#list like an array in other language
+name2 = 'liza'
+my_list = [1, "january", 1.2]
+print(my_list)
+print(name2 + " " + my_list[1])
+
+#dictionary
 x = {'name' : 'andi', 'age' : 30, 'gender' : 'male'}
 print(x)#print value of x
 print(x['name']) #print value of key name 
+#how to change value of key name
+x['name'] ='george'
+print(x)
+print(x['name'])
