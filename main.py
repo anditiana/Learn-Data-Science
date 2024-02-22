a8defbb7fe622d808a13b61bcb347f7311d31945
LittleGarden <51283033+anditiana@users.noreply.github.com>
2024-02-22 09:11:25 +0000

    Day-4 Looping
    Looping :
    1. For
    2. While
    3. For else
    4. List Comprehension

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
-word = "excelent"
-print(word)
-#change to uppercase
-word = word.upper()
-print(word)
+#Looping
 
-#change to lowercase
-word = word.lower()
-print(word)
+var_list = [1,2,3,4,5,6,7,8,9,10]
+var_char = ['A','B','C','D','E']
+for i in var_list:
+ print(i)
 
-#how to delete whitespace or other char or word
-#if whitescape on right use .rstrip()
-#if whitespace on left use .lstrip()
-#if want to delete on both side use strip()
-#if want to delete a char or word use variable.strip('word wan to delete') this only work if the word only in start
-#or end of line.
-greeting = "good morning folks 123"
-#i want to delete 123 from greeting
-print(greeting.strip(" 123 "))
+print("\n")
+for i in var_char :
+ print(i)
+
+print('\n')
+#for in range
+#range(start, stop, step)
+#default range(stop) or range(star,stop)
+#stop not include
+for i in range(1, 20, 2):
+ print(i)
+
+print("==============")
+#======================================
+#while
+
+counter = 1
+while counter <= 5:
+ print(counter)
+ counter += 1
 
 
-#join string
-print(' '.join(['Hello','World','Happy','Holiyay']))
+#for else
+number = [1,2,3,4,5]
+for num in number :
+ if num == 9:
+  print(f"w get {num}")
+  break
+else :
+ print('number not found')
 
-#to split string
-print('Hello world happy fasting'.split())
-#this method split string into some string and save into list
+#if num has 3, program will execute w get 3, and then looping stop, else print number not found
+print("==============")
 
-#to change specific char or word
-word3 = "burger, i burger like sandwich and burger"
-print(word3.replace("burger", "Pizza"))
+#list comprehension 
+num1 = [1, 2, 3, 4]
+sq = []
+for n in num1:
+  sq.append(n**2)
+print(sq)
+#this is old 
+#we can use this : new_;ist = [expression, for loop]
+#in this example expression is n**2
+print("new style")
+num2 = [1, 2, 3, 4]
+sq2 = [n**2 for n in num2]
+print(sq2)
