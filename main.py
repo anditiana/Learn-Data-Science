55eb810cc5b4536373a044fc91369b48a83d2851
LittleGarden <51283033+anditiana@users.noreply.github.com>
2024-02-26 09:45:56 +0000

    Implement of Array
    to implement array i decide to solve an array problem,
    were an array from 0 to 101, and to determine average of array

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
-#array
+#Day-5-Array Challenge
 '''
-there is 2 method to use array,
-first we can use list as array
-or, we can add module array i.e from NumPy
+there is an array from 0 to 101
+calculate the average value of array
+store the value in result
 '''
 
+var_array = [i for i in range(0,101)]
 
-#array using list
+temp_value = 0
+for i in range(len(var_array)):
+ temp_value += var_array[i]
 
-var_array = [1,2,3,4,5,6]
-length_arr = len(var_array)
-print(f"Array = {var_array}")
-print(f"Array length = {length_arr}")
+result = temp_value/len(var_array)
 
-#we can use list comperhension to set default value
-var_arr = [0 for i in range(10)]
-print(var_arr)
-
-#and then we can set value 
-for index in range(10):
- var_arr[index] = index + 1
-
-print(f"New Array = {var_arr}")
-
-
-#array exercise
-#find a highest number in array
-my_arr = [1,7,2,89,3]
-left_pointer = my_arr[0]
-
-for i in range(1, len(my_arr)):
- right_pointer = my_arr[i]
- if right_pointer > left_pointer:
-  left_pointer = right_pointer
-
-print(left_pointer)
-  
+print(result)
