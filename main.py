b5082c1e4746a0e5fb9df1df957bc05afca9d04a
LittleGarden <51283033+anditiana@users.noreply.github.com>
2024-03-05 03:01:12 +0000

    Inheritance Quiz
    simple inheritance quiz.
    make a cat class which is an inheritance of animal class.
    and have instance with name = Neko, age = 3, species = Persian

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
-#Inheritance
-#Parent Class
-class Car:
- def __init__(self, color, brand, speed):
-  self.color = color
-  self.brand = brand
-  self.speed = speed
-
- def increase_speed(self):
-  self.speed += 20
-
-car_1 = Car("Black","BMW",150)
-print("=========Basic Car===========")
-print(f"Car speed : {car_1.speed} km/h")
-car_1.increase_speed()
-print(f"After Increase Speed : {car_1.speed} km/h")
-
-
-#add child class
-
-class SportCar(Car):
- def turbo(self):
-  self.speed += 50
-
- """
- override
- add same method increase_speed as parent class but different action
- """
- def increase_speed(self):
-  self.speed += 30
-
- """
- but if we want to use same method and attribute as parent class we can use 
- super() instead write it from scratch
- 
- syntax : super().method_name_from_parent_class()
-
- def increase_speed(self):
-  super().increase.speed()
- """
-  
-car_sport_1 = SportCar("Red", "Ferrari", 200)
-print("=========Sport Car===========")
-print(f"Normal Speed : {car_sport_1.speed} km/h")
-
-#using method turbo
-car_sport_1.turbo()
-print(f"Turbo Speed : {car_sport_1.speed} km/h ")
-
-car_sport_1.increase_speed()
-print(f"After Increase Speed : {car_sport_1.speed}km/h \n=====================")
-
-
+#inheritance quiz
 """
- the result of car_sport_1.speed is 280, why not 230?
- because turbo method have called first that make current speed is 250
- then we call method increase_speed() that means we add current speed is 250 + 30 = 280 
+ 1. make a class name Animal with following conditions:
+    #have a properties :
+     - name
+     - age
+     - species
+    # have constructor :
+     - name
+     - age
+     - species
+
+ 2. make a class name Cat
+    # Cat is child class from Animal
+    # have method : 
+     - description 
+        will return : "{self.name} is cat with {self.species} species, age {self.age} years old"
+     - sound 
+        will return "meow!"
+
+ 3. make instance from Cat named "myCat" with following :
+     - name = Neko
+     - age = 3
+     - species = Persian
 """
+
+
+class Animal:
+ def __init__(self, name, age, species):
+  self.name = name
+  self.age = age
+  self.species = species
+
+
+class Cat(Animal):
+ def description(self):
+  return f"{self.name} is cat with {self.species} species, age {self.age} years old"
+
+ def sound(self):
+  return "meow!"
+
+
+myCat = Cat("Neko", 3, "Persian")
+
