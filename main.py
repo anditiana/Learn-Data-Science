0edf41a9553568e3a6224198e9660fe4d36c9c41
LittleGarden <51283033+anditiana@users.noreply.github.com>
2024-03-07 02:16:48 +0000

    simple unittest script
    i write simple unittest scipt, with out case

--- main.py
+++ main.py
@@ -1,44 +1,21 @@
-#inheritance quiz
-"""
- 1. make a class name Animal with following conditions:
-    #have a properties :
-     - name
-     - age
-     - species
-    # have constructor :
-     - name
-     - age
-     - species
+import unittest
 
- 2. make a class name Cat
-    # Cat is child class from Animal
-    # have method : 
-     - description 
-        will return : "{self.name} is cat with {self.species} species, age {self.age} years old"
-     - sound 
-        will return "meow!"
+class TestStringMethods(unittest.TestCase):
+ def test_strip(self):
+  self.assertEqual('www.dicoding.com'.strip('c.mow'), 'dicoding')
 
- 3. make instance from Cat named "myCat" with following :
-     - name = Neko
-     - age = 3
-     - species = Persian
-"""
+ def test_isalnum(self):
+  self.assertTrue('c0d1ng'.isalnum())
+  self.assertFalse('c0d!ng'.isalnum())
 
+ # Test case ketiga (3)
+ def test_index(self):
+  s = 'dicoding'
+  self.assertEqual(s.index('coding'), 2)
+  # cek s.index gagal ketika tidak ditemukan
+  with self.assertRaises(ValueError):
+      s.index('decode')
 
-class Animal:
- def __init__(self, name, age, species):
-  self.name = name
-  self.age = age
-  self.species = species
-
-
-class Cat(Animal):
- def description(self):
-  return f"{self.name} is cat with {self.species} species, age {self.age} years old"
-
- def sound(self):
-  return "meow!"
-
-
-myCat = Cat("Neko", 3, "Persian")
-
+if __name__ == '__main__':
+ # Test Runner
+ unittest.main()
